7d78f0f30a7cb4e076c859edbc81c676e3356ffd
Kevin Eales <manbehindthemadness@gmail.com>
2020-07-27 07:58:52 -0400

    Initial folder structure and requirements.

---
+++ stage/tests/adc.py
@@ -0,0 +1,12 @@
+from ADCPi import ADCPi
+
+adc = ADCPi(0x68, 0x69, 18)
+adc.set_pga(1)  # Set gain
+adc.set_bit_rate(12)  # Adjust timing (lower is faster)
+adc.set_conversion_mode(1)  # Set continuous conversion
+
+while True:
+        value = adc.read_voltage(1)
+        if value == 0.002471:  # Clamp noise
+                value = 0.0
+        print(value)  # Read Value
