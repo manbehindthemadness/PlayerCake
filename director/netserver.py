ef50e84a2ad8814f891482e859ce6ef28dbeca56
Kevin Eales <manbehindthemadness@gmail.com>
2020-07-27 10:08:20 -0400

    UDP example code working.

--- director/netserver.py
+++ director/netserver.py
@@ -1,21 +1,24 @@
 """
 This is the network server module, it contains both TCP and UDP servers.
-Reference: https://github.com/ninedraft/python-udp/blob/master/client.py
+Reference:
+    https://github.com/ninedraft/python-udp/blob/master/client.py
+    https://stackoverflow.com/questions/43475468/windows-python-udp-broadcast-blocked-no-firewall
 """
 import socket
 import time
+from director import settings
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-# Enable port reusage so we will be able to run multiple clients and servers on single (host, port).
-# Do not use socket.SO_REUSEADDR except you using linux(kernel<3.9): goto https://stackoverflow.com/questions/14388706/how-do-so-reuseaddr-and-so-reuseport-differ for more information.
-# For linux hosts all sockets that want to share the same address and port combination must belong to processes that share the same effective user ID!
-# So, on linux(kernel>=3.9) you have to run multiple servers and clients under one user to share the same (host, port).
 # Thanks to @stevenreddie
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+# server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # Enable broadcasting mode
+# server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+server.bind((settings.BindAddr, 37020))  # This is a windows thing...
 
 # Set a timeout so the socket does not block
 # indefinitely when trying to receive data.
---
+++ director/settings.py
@@ -0,0 +1,5 @@
+"""
+This is the software configuration settings file. These values are general defaults and should be modified in the logal_settings file not here!
+"""
+
+BindAddr = '10.4.222.51'
