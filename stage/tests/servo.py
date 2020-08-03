7a7d3fde918dddddfd54c9f66f90bc48c6369943
Kevin Eales <manbehindthemadness@gmail.com>
2020-08-03 10:44:02 -0400

    Revised servo test.

--- stage/tests/servo.py
+++ stage/tests/servo.py
@@ -6,15 +6,25 @@ from adafruit_servokit import ServoKit
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
 kit = ServoKit(channels=16)
 cnt = 0
+range_ = list(range(30, 120))  # type: list
 
-while cnt < 10:
-    for servo in range(1, 15):
-        kit.servo[servo].angle = 180
-    time.sleep(1)
-    for servo in range(1, 15):
-        kit.servo[servo].angle = 0
-    time.sleep(1)
-    cnt += 1
+while cnt < 5:
+    for position in range_:
+        for servo in range(1, 15):
+            kit.servo[servo].angle = position
+            print(servo, position)
+            time.sleep(0.01)
+    # time.sleep(1)
+    range_.reverse()
+    for position in range_:
+        for servo in range(1, 15):
+            kit.servo[servo].angle = position
+            time.sleep(0.01)
+    # time.sleep(1)
+    # cnt += 1
+    # if cnt == 5:
+    #     for servo in range(1, 15):
+    #         kit.servo[servo].angle = 90
 
 # kit.servo[14].angle = 180
 # kit.continuous_servo[1].throttle = 1.0
