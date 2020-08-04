fbc21db9771f224f6a6716f9dc393ed6f60983bc
Kevin Eales <manbehindthemadness@gmail.com>
2020-08-04 10:24:00 -0400

    Added oem berry imu files

---
+++ stage/oem/berry_imu/berryIMU-measure-G.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+#
+#       This program demonstrates how to convert the raw values from an accelerometer to Gs
+#
+#
+#       Both the BerryIMUv1 and BerryIMUv2 are supported
+#
+#       This script is python 2.7 and 3 compatible
+#
+#       Feel free to do whatever you like with this code.
+#       Distributed as-is; no warranty is given.
+#
+#       http://ozzmaker.com/
+
+
+
+import time
+import IMU
+
+
+
+
+IMU.detectIMU()     #Detect if BerryIMUv1 or BerryIMUv2 is connected.
+IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
+
+
+
+
+while True:
+
+
+    #Read the accelerometer,gyroscope and magnetometer values
+    ACCx = IMU.readACCx()
+    ACCy = IMU.readACCy()
+    ACCz = IMU.readACCz()
+    yG = (ACCx * 0.244)/1000
+    xG = (ACCy * 0.244)/1000
+    zG = (ACCz * 0.244)/1000
+    print("##### X = %fG  ##### Y =   %fG  ##### Z =  %fG  #####" % ( yG, xG, zG))
+
+
+
+    #slow program down a bit, makes the output more readable
+    time.sleep(0.03)
