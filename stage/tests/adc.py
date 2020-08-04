498ab5f6d3cfb0a302ea9f9c2abdaef667573c1f
Kevin Eales <manbehindthemadness@gmail.com>
2020-08-04 10:24:14 -0400

    Revised the ADC test.

--- stage/tests/adc.py
+++ stage/tests/adc.py
@@ -11,7 +11,19 @@ adc.set_bit_rate(12)  # Adjust timing (lower is faster)
 adc.set_conversion_mode(1)  # Set continuous conversion
 
 while True:
-    value = adc.read_voltage(1)
-    if value == 0.002471:  # Clamp noise
-        value = 0.0
-    print(value)  # Read Value
+    output = list()
+    values = [
+        adc.read_voltage(1),
+        adc.read_voltage(2),
+        adc.read_voltage(3),
+        adc.read_voltage(4),
+        adc.read_voltage(5),
+        adc.read_voltage(6),
+        adc.read_voltage(7),
+        adc.read_voltage(8),
+    ]
+    for value in values:
+        if value == 0.0:  # 0.002471:  # Clamp noise
+            value = 0.0
+        output.append(value)
+    print(output)
