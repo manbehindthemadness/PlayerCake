8323fa43c3183ea9b24bff6b324e3bf1e18adb7d
Kevin Eales <manbehindthemadness@gmail.com>
2020-08-31 10:32:24 -0400

    Added image resizer so we will always have crisp graphics.

--- warehouse/utils.py
+++ warehouse/utils.py
@@ -4,6 +4,10 @@ This is where we house general utilities.
 import datetime
 import re
 from colour import Color
+from PIL import Image
+from resizeimage import resizeimage
+from os import path
+from pathlib import Path
 
 
 def to_color(limit_low, limit_high):
@@ -124,3 +128,103 @@ def split_string(string):
     This splits a sring at any non-alphanumeric chars.
     """
     return re.split('[^a-zA-Z]', string)
+
+
+def ceiling(x):
+    """
+    Generic math method (rounds up int's instead of down)
+    """
+    n = int(x)
+    return n if n - 1 < x <= n else n + 1
+
+
+def percent_of(percent, whole, use_float=False):
+    """
+    Generic math method
+    """
+    if not percent or not whole:
+        result = 0
+    else:
+        result = (percent * whole) / 100.0
+        if not use_float:
+            result = ceiling(result)
+    return result
+
+
+def percent_in(part, whole, use_float=False):
+    """
+    Generic math method
+    """
+    if not whole:
+        result = 0
+    else:
+        result = 100 * part / whole
+        if not use_float:
+            result = ceiling(result)
+    return result
+
+
+def file_rename(name, file):
+    """
+    This appends 'name' to a filename 'file' whilst preserving the extension.
+    :type name: str
+    :type file: str
+    :rtype: str
+    """
+    pa, fi = file.rsplit('.', 1)
+    name += '.'
+    return pa + name + fi
+
+
+def file_exists(file):
+    """
+    Checks to see if a specific file exists.
+    :type file: str
+    :rtype: bool
+    """
+    return path.exists(file)
+
+
+def image_resize(x, y, image, x_percent, y_percent, preserve_aspect=True):
+    """
+    This resizes images to a percentage of x and y and saves it to the /img folder
+    This skips the action if the resized file already exists.
+
+    https://pypi.org/project/python-resize-image/
+    libjpeg-turbo-devel
+
+    :param x: Layout x size in pixels.
+    :type x: int
+    :param y: Layout y size in pixels.
+    :type y: int
+    :param image: Filename of the base image.
+    :type image: str
+    :param x_percent: The x percentags of the output image.
+    :type x_percent: int, float
+    :param y_percent: The y percentage of the output image.
+    :type y_percent: int, float
+    :param preserve_aspect: Toggles preservation of the aspect ratio.
+    :type preserve_aspect: bool
+    :return: Resized image's file name.
+    :rtype: str
+    """
+    base = Path.cwd()
+    x_pix = percent_of(x, x_percent)
+    y_pix = percent_of(y, y_percent)
+    tp = base / Path('img/resize')
+    tfn = tp / file_rename(str(x_pix) + '_' + str(y_pix), image)
+    # print('target', tfn)
+    if not file_exists(tfn):
+        sp = base / Path('img/base')
+        sfn = sp / image
+        # print('source', sfn)
+        if file_exists(sfn):
+            img = Image.open(sfn)
+            if preserve_aspect:
+                img = resizeimage.resize_contain(img, [x_pix, y_pix])
+            else:
+                img = resizeimage.resize_cover(img, [x_pix, y_pix], validate=False)
+            img.save(tfn, img.format)
+        else:
+            raise FileNotFoundError
+    return tfn
--- writer/start.py
+++ writer/start.py
@@ -2,24 +2,62 @@
 This is the main program loop for the writer application.
 
 Tops for messed up X: https://www.ibm.com/support/pages/x11-forwarding-ssh-connection-rejected-because-wrong-authentication
+NOTE: Copy the .Xauthority file from root to pi.
 
 NOTE: THE X-SERVER MUST BE RUNNING!
 """
 
-from guizero import App, Box, Text
+from tkinter import *
+from tkinter import ttk
 from warehouse.system import system_command
-from warehouse import settings
+from writer import settings
 import os
 
 scr_x, scr_y = settings.screensize
 
+# This crap is for tunneling the app over ssh
 os.environ['DISPLAY'] = 'localhost:11.0'
+os.environ['XAUTHORITY'] = '/home/pi/.Xauthority'
 system_command(['/usr/bin/xhost', '+'])
 
-app = App()
-box = Box(app, width=150, height=150)
-box.bg = "red"
-box.border = True
 
-text = Text(box, text="hello")
-app.display()
+def calculate(*args):
+    """something"""
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+
+root = Tk()
+root.title("PlayerCake")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+feet = StringVar()
+meters = StringVar()
+
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+button_style = ttk.Style()  # This is the magic right here.
+button_style.configure("b1.TButton", borderwidth=0)
+
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate, style='b1.TButton').grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind('<Return>', calculate)
+
+root.mainloop()
